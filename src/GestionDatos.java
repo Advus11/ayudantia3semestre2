b9ce16520c94d9046a48d529ac07551b481a8d4b
Adonis Subiabre <a.subiabre01@ufromail.cl>
2023-09-07 09:57:40 -0300

    commit2 con cambios -Si se ingresa un nuevo empleado se agrega normalmente -Si esta repetido no se agrega

--- src/GestionDatos.java
+++ src/GestionDatos.java
@@ -7,12 +7,28 @@ public class GestionDatos {
     private static List<Empleado> empleados = new ArrayList<>();
 
     public static void escribirEmpleados() {
-        try (PrintWriter writer = new PrintWriter(new FileWriter(NOMBRE_ARCHIVO))) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(NOMBRE_ARCHIVO, true))) {
             for (Empleado empleado : empleados) {
-                if (empleado instanceof EmpleadoAsalariado) {
-                    writer.println(empleado.getNombre() + ",Asalariado," + empleado.calcularSalario());
-                } else if (empleado instanceof EmpleadoPorHoras) {
-                    writer.println(empleado.getNombre() + ",Por Horas," + empleado.calcularSalario());
+                boolean empleadoExiste = false;
+                try (BufferedReader reader = new BufferedReader(new FileReader(NOMBRE_ARCHIVO))) {
+                    String linea;
+                    while ((linea = reader.readLine()) != null) {
+                        String[] partes = linea.split(",");
+                        if (partes.length >= 1 && partes[0].equals(empleado.getNombre())) {
+                            empleadoExiste = true;
+                            break;
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                if (!empleadoExiste) {
+                    if (empleado instanceof EmpleadoAsalariado) {
+                        writer.println(empleado.getNombre() + ",Asalariado," + empleado.calcularSalario() + ",Activo");
+                    } else if (empleado instanceof EmpleadoPorHoras) {
+                        writer.println(empleado.getNombre() + ",Por Horas," + empleado.calcularSalario() + ",Activo");
+                    }
                 }
             }
             System.out.println("Datos de empleados escritos en el archivo " + NOMBRE_ARCHIVO);
@@ -27,10 +43,17 @@ public class GestionDatos {
             System.out.println("Nombres, Tipo y Salarios de empleados almacenados en el archivo:");
             while ((linea = reader.readLine()) != null) {
                 String[] partes = linea.split(",");
-                if (partes.length == 3) {
+                if (partes.length >= 4 && partes[3].equals("Activo")) {
                     String nombre = partes[0];
                     String tipo = partes[1];
                     double salario = Double.parseDouble(partes[2]);
+                    if (!empleadoYaRegistrado(nombre, salario)) {
+                        if (tipo.equals("Asalariado")) {
+                            empleados.add(new EmpleadoAsalariado(nombre, salario));
+                        } else if (tipo.equals("Por Horas")) {
+                            empleados.add(new EmpleadoPorHoras(nombre, salario, 0.0)); // Puedes establecer horas en 0, ya que no está en el archivo
+                        }
+                    }
                     System.out.println("Nombre: " + nombre + " | Tipo: " + tipo + " | Salario: " + salario);
                 }
             }
@@ -40,27 +63,33 @@ public class GestionDatos {
     }
 
     public static void main(String[] args) {
-        EmpleadoAsalariado empleadoAsalariado = new EmpleadoAsalariado("Juan", 3000);
-        EmpleadoPorHoras empleadoPorHoras = new EmpleadoPorHoras("Maria", 15, 40);
+        leerEmpleados(); 
+        EmpleadoAsalariado empleadoAsalariado = new EmpleadoAsalariado("Juana2run", 4000);
+        EmpleadoPorHoras empleadoPorHoras = new EmpleadoPorHoras("Maria", 20, 45);
 
-        // Agregar empleados a la lista
-        empleados.add(empleadoAsalariado);
-        empleados.add(empleadoPorHoras);
-
-        // Agregar más empleados si es necesario
-        EmpleadoAsalariado nuevoAsalariado = new EmpleadoAsalariado("Ana", 3500);
-        empleados.add(nuevoAsalariado);
-
-        EmpleadoAsalariado nuevoAsalariado2 = new EmpleadoAsalariado("Adonis", 4000);
-        empleados.add(nuevoAsalariado2);
-
-        EmpleadoPorHoras nuevoPorHoras = new EmpleadoPorHoras("Carlos", 12, 45);
-        empleados.add(nuevoPorHoras);
+        agregarEmpleadoSiNoExiste(empleadoAsalariado);
+        agregarEmpleadoSiNoExiste(empleadoPorHoras);
 
         escribirEmpleados();
-        leerEmpleados();
+    }
+
+    private static void agregarEmpleadoSiNoExiste(Empleado empleado) {
+        if (!empleados.contains(empleado)) {
+            empleados.add(empleado);
+        }
+    }
+
+    private static boolean empleadoYaRegistrado(String nombre, double salario) {
+        for (Empleado empleado : empleados) {
+            if (empleado.getNombre().equals(nombre) && empleado.calcularSalario() == salario) {
+                return true;
+            }
+        }
+        return false;
     }
 }
+
+
 
 
 
